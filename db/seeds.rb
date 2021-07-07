b1e245bd84fd4d8b758b84f3b4bfcfdac44c0498
Bill <billfeng2002@gmail.com>
2021-07-06 21:47:50 -0700

    commit tree 37fa583570b7b69a1c4c2032bd8ae38698e2aa64

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do |i|
+    a=Artist.create(name: "Bill#{i}")
+end
+
+20.times do |i|
+    s=Song.create(title: "Song#{i}", artist_id: Artist.ids.sample)
+end
